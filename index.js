f6696fcf05670b510c6e9f65b0ef15a7b655fea9
Noura Loudani <contact.nouraloudani@gmail.com>
2020-09-23 05:45:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function yourAnimal() {
 }
 
 const two = 2
+
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
