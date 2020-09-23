908cd1a455ca1846f23f7871d638616331b1eb22
Noura Loudani <contact.nouraloudani@gmail.com>
2020-09-23 05:26:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  return animal
   var animal = 'cat'
+  return animal
+
 }
 
 function add2(n) {
